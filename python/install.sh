f5b25f05ed2c73c52896392bdc8f1ae29d69393c
Jasper <jaspertax@gmail.com>
2023-02-20 23:44:07 +0100

    Fixed pyenv halting install script
    pyenv asked for confirmation from user if python version already
    installed, which was not displayed and thus halting the install and
    skipping other install scripts. This should at least prevent an existing
    version from being installed.

--- python/install.sh
+++ python/install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pyenv install 3.11
+
+pythonversion='3.11'
+
+if pyenv versions | grep -q $pythonversion
+then
+  echo "python version $pythonversion is already installed"
+else
+  echo "installing python version $pythonversion"
+  pyenv install 3.11
+fi
+echo "setting $pythonversion as default"
 pyenv global 3.11
