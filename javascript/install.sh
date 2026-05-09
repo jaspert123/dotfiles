0c2287e4fbb1cddc4c89aa2187f3efa2c6f57bb7
Jasper <jaspertax@gmail.com>
2026-05-10 01:01:08 +0200

    Node install

--- javascript/install.sh
+++ javascript/install.sh
@@ -1,1 +1,13 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+nodeversion='24.15.0'
+
+if nvm ls | grep -q $nodeversion
+then
+  echo "node version $nodeversion is already installed"
+else
+  echo "installing node version $nodeversion"
+  nvm install $nodeversion 
+fi
+echo "setting $nodeversion as default"
+nvm use $nodeversion
