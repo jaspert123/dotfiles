db9604a21650ff1b09eea90486c7fc12945637f5
Jasper <jaspertax@gmail.com>
2026-05-09 22:07:59 +0200

    grc is now installed by mac ports

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
+if (( $+commands[grc] )) && (( $+commands[port] ))
 then
-  source `brew --prefix`/etc/grc.zsh
+  source /opt/local/etc/grc.d/grc.zsh
 fi
+
